6d144a9752938d659add192878d2c914ee11c62d
JingYu Ning <115336923+jrunkening@users.noreply.github.com>
2022-12-12 21:10:19 +0800

    Enhance docs

--- pytemplate/rock_paper_scissors.py
+++ pytemplate/rock_paper_scissors.py
@@ -12,6 +12,23 @@ class Gesture:
         self.result_rep = {0: "tie", 1: "win", 2: "loss"}
 
     def compare(self, g: "Gesture"):
+        """
+        Compare with another `Gesture`.
+
+        Args:
+            * `self` (`Gesture`): A gesture.
+            * `g` (`Gesture`): Another gesture.
+
+        Returns:
+            * `result` (`str`): "win", "loss" or "tie".
+
+        Examples:
+            >>> g0 = Gesture(0)
+            >>> g1 = Gesture(1)
+            >>> g0.compare(g1)
+            'loss'
+        """
+
         result = (3 + self.id - g.id) % 3
 
         return self.result_rep[result]
@@ -24,15 +41,25 @@ class Gesture:
 
 
 def get_computer_gesture():
+    """
+    Get a random gesture that represents computer.
+
+    Returns:
+        * (`Gesture`): A random gesture that represents computer.
+    """
+
     return Gesture(random.randint(0, 3))
 
 
 def play(gesture_u: "Gesture"):
     """
-    play(gesture_u: "Gesture")
-    Play mora with computer
-    :args gesture_u: a `Gesture`
-    :return result: "win", "loss" or "tie
+    Play mora with computer.
+
+    Args:
+        * `gesture_u` (`Gesture`): A gesture that represents user.
+
+    Returns:
+        * `result` (`str`): "win", "loss" or "tie".
     """
 
     gesture_c = get_computer_gesture()
--- pytemplate/untested.py
+++ pytemplate/untested.py
@@ -1,4 +1,11 @@
 def untested_func():
+    """
+    This function is not tested on purpose.
+
+    Returns:
+        * `3` (`int`): A constant for nonsense.
+    """
+
     a = 1
     b = 2
 
